fab61ba01410f7ca3c80036f2188537fd8dd14ec
IanfvBR <ian96fv@gmail.com>
2026-03-03 19:11:48 -0300

    Todo o código foi reescrito
    Com o período de rematrícula aberto, foi possível testar o que estava sendo feito, ocasionando em profundas mudanças no código.

--- scripts/background-script.js
+++ scripts/background-script.js
@@ -1,8 +1,13 @@
-if (!browser.action.onClicked.hasListener(listener)) {
-    browser.action.onClicked.addListener(listener);
-}
-
-function listener(tab) {
-    browser.scripting.executeScript("./scripts/content-script.js");
-}
+browser.action.onClicked.addListener(async (tab) => {
+  try {
+    await browser.scripting.executeScript({
+      target: {
+        tabId: tab.id,
+      },
+      files: ["./scripts/content-script.js"],
+    });
+  } catch (err) {
+    console.error(`failed to execute script: ${err}`);
+  }
+});
 
